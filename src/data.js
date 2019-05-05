cb6e5d17f647e1f4407c8eec1c608129e5f6b527
PaulaTorresS <paula.torres.salazar@gmail.com>
2019-05-05 09:17:53 -0400

    funcion filtrar

--- src/data.js
+++ src/data.js
@@ -3,8 +3,30 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
+//Funcion para Filtrar por Nombre y Numero
+window.pokemonData = {
+  byName: (data,condition) => {
+    let result = [];
+    for (let i = 0; i < data.length; i++){
+      if (data[i].name === condition){
+        result.push(data[i]);
+        }
+      else if (data[i].id === parseInt(condition)){
+          result.push(data[i]);
+      } 
 
-window.example = example;
+    } return result;
+    
+  },
+  //Funcion para Filtrar por Tipo
+    byType: (data,condition) => {
+    let result = [];
+    for (let i = 0; i < data.length; i++){
+      for (let j = 0; j < 2; j++){
+        if (data[i].type[j] === condition){
+          result.push(data[i]);
+        }
+      }
+    }return result;
+  }
+};
