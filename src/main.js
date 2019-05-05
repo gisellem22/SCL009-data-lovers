60493a5fc05c5a05b05ff42dcbe02ad1ef9f10cf
PaulaTorresS <paula.torres.salazar@gmail.com>
2019-05-05 10:04:22 -0400

    Funcion Filtrar + Test aprovados

--- src/main.js
+++ src/main.js
@@ -1,27 +1,104 @@
-/* Manejo del DOM */
-
-/*let allPokemon = POKEMON.pokemon;
-let name = ["Raichu"];
-
-const pokemonFilterByName = (name) => {
-    for (let a = 0; a < allPokemon.length; a++){
-        if (allPokemon[a].name === name){
-            name.push(allPokemon[a]);
-        }
-    }
-    return name;
-};
-console.log(pokemonFilterByName(name));*/
-
-let allPokemon = POKEMON.pokemon;
-let containerSearch = document.getElementById("search").value;
+//Declaración de Variables
+const allPokemon = window.POKEMON.pokemon;
 let containerBtnSearch = document.getElementById("btn_search");
-let name;
+let containerResult = document.getElementById("result");
+let search;
+// let containerAZ = document.getElementById("a_z");
+// let containerZA = document.getElementById("z_a");
+// let container1to151 = document.getElementById("number1to151");
+// let container151to1 = document.getElementById("number151to1");
+let containerSelect = document.getElementById("select_type");
+let cardHTML;
+
+//Función Crear Elementos html
+const showElements = (data) => {
+  data.forEach(element => {
+    cardHTML +=
+     `<div class="card" style="width: 200px;">
+  <img class="card-img-top" src=${element.img} alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">${element.name}</h5>
+    <p class="card-text">#${element.num}.</p>
+    <p class="card-text">Tipo: ${element.type}.</p>
+  </div>
+</div>`
+  })
+ containerResult.innerHTML = cardHTML;
+};
+//Botón Search Por Nombre
 containerBtnSearch.addEventListener("click", () => {
-name = containerSearch;
+    cardHTML = "";
+    search = document.getElementById("search").value;
+    showElements(window.pokemonFilter.byName(allPokemon, search));
+    showElements(window.pokemonFilter.byNumber(allPokemon, search));
+    showElements(window.pokemonFilter.byType(allPokemon, search));
+});
+//Select de tipos
+containerSelect.addEventListener("change", () => {
+  cardHTML = "";
+  search = containerSelect.value;
+  showElements(window.pokemonFilter.byType(allPokemon, search));
+}); 
 
-console.log(name, allPokemon);
-console.log(window.pokemonIChooseU.pokemonFilterByName(name));
-}
-);
+// //Botón Ordenar por Nombre de A a Z
+// containerAZ.addEventListener("click", () => {
+//   let orderedAZ = az();
+//   console.log (orderedAZ);
+//   createElement(orderedAZ);
+// });
 
+// //Botón Ordenar por Nombre de Z a A
+// containerZA.addEventListener("click", () => {
+//   let orderedZA = za();
+//   console.log (orderedZA);
+//   createElement(orderedZA);
+// });
+
+// //Botón Ordenar por Numero de 1 a 151
+// container1to151.addEventListener("click", () => {
+//   createElement(allPokemon);
+//   console.log (allPokemon);
+// });
+
+// // //Botón Ordenar por Numero de 151 a 1
+// container151to1.addEventListener("click", () => {
+//   let from151to1 = order151to1();
+//   console.log (from151to1);
+//   createElement(from151to1);
+// });
+
+// //Función Ordenar de A a Z
+// const az = () => {
+//   return allPokemon.sort((a,b) => {
+//    a.name < b.name }
+//    )};
+
+// //Función  Ordenar de Z a A
+// const za = () => {
+//   return allPokemon.sort((c,d) => {
+//     if (c.name > d.name) 
+//    return -1;
+//   if (c.name < d.name)
+//    return 1;
+//   return 0; 
+//  })};
+
+// //Función  Ordenar de 151 a 1
+// const order151to1 = () => {
+//   return allPokemon.sort((c,d) => {
+//     if (c.id > d.id) 
+//    return -1;
+//   if (c.id < d.id)
+//    return 1;
+//   return 0; 
+//  })};
+ 
+//    //Función Crear Elementos html
+// let createElement = (data) => {  
+//   for (let b= 0; b < data.length; b++){
+//     let newDiv = document.createElement("IMG");
+//     newDiv.setAttribute("src",data[b].img);
+//     document.getElementById("result").appendChild(newDiv);
+//   }
+// };
+window.allPokemon = allPokemon;
--- test/data.spec.js
+++ test/data.spec.js
@@ -4,13 +4,125 @@ require('../src/data');
 require('./data.spec.js');
 
 
-describe('example', () => {
+describe('pokemonData', () => {
   
-  it('debería ser una función', () => {
-    assert.equal(typeof example, 'function');
+  it('debería ser un objeto', () => {
+    assert.equal(typeof pokemonData, 'object');
   });
 
-  it('debería retornar "example"', () => {
-    assert.equal(example(), 'example');
+  describe('pokemonData.byName', () => {
+    const data = [{
+      "id": 1,
+      "num": "001",
+      "name": "Bulbasaur",
+      "img": "http://www.serebii.net/pokemongo/pokemon/001.png",
+      "type": [
+        "Grass",
+        "Poison"
+      ],
+      "height": "0.71 m",
+      "weight": "6.9 kg",
+      "candy": "Bulbasaur Candy",
+      "candy_count": 25,
+      "egg": "2 km",
+      "spawn_chance": 0.69,
+      "avg_spawns": 69,
+      "spawn_time": "20:00",
+      "multipliers": [1.58],
+      "weaknesses": [
+        "Fire",
+        "Ice",
+        "Flying",
+        "Psychic"
+      ],
+      "next_evolution": [{
+        "num": "002",
+        "name": "Ivysaur"
+      }, {
+        "num": "003",
+        "name": "Venusaur"
+      }]
+    }, {
+      "id": 2,
+      "num": "002",
+      "name": "Ivysaur",
+      "img": "http://www.serebii.net/pokemongo/pokemon/002.png",
+      "type": [
+        "Grass",
+        "Poison"
+      ],
+      "height": "0.99 m",
+      "weight": "13.0 kg",
+      "candy": "Bulbasaur Candy",
+      "candy_count": 100,
+      "egg": "Not in Eggs",
+      "spawn_chance": 0.042,
+      "avg_spawns": 4.2,
+      "spawn_time": "07:00",
+      "multipliers": [
+        1.2,
+        1.6
+      ],
+      "weaknesses": [
+        "Fire",
+        "Ice",
+        "Flying",
+        "Psychic"
+      ],
+      "prev_evolution": [{
+        "num": "001",
+        "name": "Bulbasaur"
+      }],
+      "next_evolution": [{
+        "num": "003",
+        "name": "Venusaur"
+      }]
+    }, {
+      "id": 3,
+      "num": "003",
+      "name": "Venusaur",
+      "img": "http://www.serebii.net/pokemongo/pokemon/003.png",
+      "type": [
+        "Grass",
+        "Poison"
+      ],
+      "height": "2.01 m",
+      "weight": "100.0 kg",
+      "candy": "Bulbasaur Candy",
+      "egg": "Not in Eggs",
+      "spawn_chance": 0.017,
+      "avg_spawns": 1.7,
+      "spawn_time": "11:30",
+      "multipliers": null,
+      "weaknesses": [
+        "Fire",
+        "Ice",
+        "Flying",
+        "Psychic"
+      ],
+      "prev_evolution": [{
+        "num": "001",
+        "name": "Bulbasaur"
+      }, {
+        "num": "002",
+        "name": "Ivysaur"
+      }]
+    }];
+  
+    it('debería ser una función', () => {
+      assert.equal(typeof window.pokemonData.byName, 'function');
+    });
+
+    it('debería retornar el objeto Bulbasaur al filtrar por nombre "Bulbasaur"', () => {
+      assert.deepEqual( window.pokemonData.byName(data, 'Bulbasaur'), [data[0]]);
+    });
+
+    it('debería ser una función', () => {
+      assert.equal(typeof window.pokemonData.byType, 'function');
+    });
+
+    it('debería retornar los objetos Bulbasaur, Ivysaur y Venusaur al filtrar por el tipo "Grass"', () => {
+      assert.deepEqual( window.pokemonData.byType(data, 'Grass'), [data[0],data[1],data[2]]);
+    });
   });
-})
+ });
