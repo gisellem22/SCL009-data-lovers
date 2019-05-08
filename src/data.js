758ff7aa583817c3056838acfcdab5de689fbc8c
Giselle Machado <gisellemachadoyes@gmail.com>
2019-05-08 17:19:44 -0400

    agregando el modal

--- src/data.js
+++ src/data.js
@@ -1,35 +1,23 @@
 window.pokemonData = {
   byName: (data,condition) => {
-    let result = [];
-    for (let i = 0; i < data.length; i++){
-      if (data[i].name === condition){
-        result.push(data[i]);
-      } 
-    } return result;
-    
+  return data.filter((a) => a.name === condition );
   } 
   ,
   byNumber: (data,condition) => {
-    let result = [];
-    for (let i = 0; i < data.length; i++){
-      if (data[i].id === parseInt(condition)){
-        result.push(data[i]);
-      } 
-    } return result;
+   return data.filter((a) => a.id === parseInt(condition));
   } 
   ,
   byType: (data,condition) => {
      return data.filter((a)=> a.type[0]===condition || a.type[1]===condition)
-      }
+  }
   ,
   //Función Ordenar de A a Z
   sortData: (data, sortBy, sortedOrder) => {
-    let result = [];
   if (sortedOrder === "increasing"){
-    result = data.sort((a,b) => a[sortBy].localeCompare(b[sortBy]));
+    return data.sort((a,b) => a[sortBy].localeCompare(b[sortBy]));
       } else if (sortedOrder === "decreasing"){
-    result = data.sort((a,b) => a[sortBy].localeCompare(b[sortBy])).reverse();
-      } return result;
+    return data.sort((a,b) => a[sortBy].localeCompare(b[sortBy])).reverse();
+      }
  } 
 };
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 //Declaración de Variables
 const allPokemon = window.POKEMON.pokemon;
 let containerBtnSearch = document.getElementById("btn_search");
+let showData = document.getElementById("show_data");
 let containerResult = document.getElementById("result");
 let search;
 let containerAZ = document.getElementById("a_z");
@@ -8,13 +9,16 @@ let containerZA = document.getElementById("z_a");
 let container1To151 = document.getElementById("number1to151");
 let container151To1 = document.getElementById("number151to1");
 let containerSelect = document.getElementById("select_type");
-let cardHTML;
+let containerChoose = document.getElementById("choose_pokemon");
+let containerModal = document.getElementById("show_choose");
+let cardHTML = "";
+let modalCard;
 
 //Función Crear Elementos html
 const showElements = (data) => {
   data.forEach(element => {
     cardHTML +=
-     `<div class="card " style="width: 200px;">
+     `<div class="card " style="width: 200px;" data-toggle="modal" data-target="#exampleModalCenter" onclick="showModal(${element.name})">
   <img class="card-img-top" src=${element.img} alt="Card image cap">
   <div class="card-body">
     <h5 class="card-title">${element.name}</h5>
@@ -24,7 +28,15 @@ const showElements = (data) => {
 </div>`
   })
  containerResult.innerHTML = cardHTML;
+ 
 };
+
+//Cards Aleatorios por defecto
+const radomData = (data)=>{
+  return data.sort(() => Math.random() - 0.5)
+};
+showElements(radomData(allPokemon));
+
 //Botón Search
 containerBtnSearch.addEventListener("click", () => {
     cardHTML = "";
@@ -60,3 +72,15 @@ container151To1.addEventListener("click", () => {
   cardHTML = "";
   showElements(window.pokemonData.sortData(allPokemon,"num", "decreasing"));
 });
+// containerChoose.addEventListener("click", () => {
+//   showData.style.display = (showData.style.display=="block") ?"none" : "block";
+//   containerModal.style.display = (containerModal.style.display == "none") ?"block" : "none";
+// });
+// document.getElementById("card_number").addEventListener("click", () => {
+//   showData.style.display = (showData.style.display=="block") ?"none" : "block";
+//   containerModal.style.display = (containerModal.style.display == "none") ?"block" : "none";
+// });
+const showModal = (data)=>{
+  document.getElementById("exampleModalCenterTitle").innerHTML= data;
+ 
+};
